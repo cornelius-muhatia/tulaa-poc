d999202c9d18b33d0ac15b36cffc11bc66cc9240
cornelius-binary <csakwa@binary.co.ke>
2018-06-21 17:13:27 +0300

    Working on api readme

--- tulaa-poc-api/README.md
+++ tulaa-poc-api/README.md
@@ -1,2 +1,14 @@
 # Tulaa POC API
 A web service exposing solutions to interview questions
+
+## Getting Started
+Before you get started ensure the host machine has Java JDK 1.8 or later installed. 
+You can check the installed java version from terminal by running the following command:
+
+[source,indent=0]
+----
+    java -version
+----
+
+### Prerequisites
+
--- tulaa-poc-api/src/test/java/com/cm/projects/talaa/poc/TalaaPocApplicationTests.java
+++ tulaa-poc-api/src/test/java/com/cm/projects/talaa/poc/TalaaPocApplicationTests.java
@@ -1,5 +1,9 @@
 package com.cm.projects.talaa.poc;
 
+import com.cm.projects.talaa.poc.services.StewardService;
+import com.cm.projects.talaa.poc.services.StewardServiceTemplate;
+import static org.junit.Assert.assertSame;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,9 +12,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TalaaPocApplicationTests {
+    
+    private StewardService stewardService;
+    
+     @Before
+    public void setUp() {
+        this.stewardService = new StewardServiceTemplate();
+    }
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void testReverseString() {
+        assertSame("Testing reversing string while maintaining special chars", "a,b$c", this.stewardService.reverseString("a,b$c"));
+    }
 
 }
